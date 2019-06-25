59e1fc5edf39bba201698e028a1eb740cab1eb7c
Kohei TAKATA <kt.koheitakata@gmail.com>
2019-06-25 22:28:27 +0900

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
   const [count, setCount] = useState(0);
   const onClick = () => setCount(count + 1);
+
+  useEffect(() => {
+    document.title = String(count);
+  });
+
+  useEffect(() => {
+    alert(`updated ${count} times !`);
+  }, [Math.floor(count/3)]);
+
   return (
     <div>
       <p>You clicked {count} times</p>
