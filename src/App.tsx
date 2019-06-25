7339038f4d6e782780cb859e220a69c3a7179f22
Kohei TAKATA <kt.koheitakata@gmail.com>
2019-06-25 22:14:57 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount(count + 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={onClick}>
+        Click me!
+      </button>
     </div>
   );
 }
-
-export default App;
